c0a1cb6c1af80d82b58ca7b3b646463e431d14bf
2026aim1015 <2026aim1015@iitrpr.ac.in>
2026-08-05 21:50:56 +0530

    test case

--- assignment_01/driver/dfs_driver.cpp
+++ assignment_01/driver/dfs_driver.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <chrono>
 #include <string>
+#include <fstream>
 #include "../src/graph.h"
 using namespace std;
 using namespace std::chrono;
+ofstream fout;
 
 //dfs function declaration
 void dfs(CSRGraph &g,int src);
@@ -27,6 +29,10 @@ int main()
     //read the source vertex
     cout<<"Enter source vertex : ";
     cin>>src;
+    fout.open("outputs/dfs_output.txt");
+    fout << "Algorithm: DFS" << endl;
+    fout << "Source: " << src << endl;
+    fout << "Traversal: ";
     //start time 
     auto start = high_resolution_clock::now();
     //calling dfs function
@@ -38,9 +44,10 @@ int main()
     //display exceution time
     double time = t.count() / 1000.0;   // Convert microseconds to milliseconds
 
-    cout << "Execution time : "
-        << time
-        << " ms" << endl;
+    cout << "Execution time : "<<time<<" ms"<<endl;
 
+    fout << endl;
+    fout << "Execution time : "<<time<<" ms"<< endl;
+    fout.close();
     return 0;
 }
